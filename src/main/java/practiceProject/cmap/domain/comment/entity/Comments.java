8602dfca3d7127d4d855cd870255f118784cfa24
stpark0059 <stpark0509@gmail.com>
2024-06-04 22:35:51 +0900

    Comments, MemberLikeBoard Entity 완료

---
+++ src/main/java/practiceProject/cmap/domain/comment/entity/Comments.java
@@ -0,0 +1,37 @@
+package practiceProject.cmap.domain.comment.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import practiceProject.cmap.config.BaseEntity.BaseTimeEntity;
+import practiceProject.cmap.domain.board.entity.Board;
+import practiceProject.cmap.domain.member.entity.Member;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+public class Comments extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Lob
+    private String body;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+}
